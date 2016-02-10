bd9a6b6f15032d6a21649b7f6067bfb89ce2d5d7
Diego Martinez <Diego.ma.martinez@gmail.com>
2016-02-10 02:15:02 -0500

    define data

--- JS/array/array.js
+++ JS/array/array.js
@@ -1,1 +1,16 @@
-s
+/*Define Working Data*/
+var cities = ["Warren","Washington","Wilkes-Barre", "Williamsport","York"]
+
+
+
+
+
+
+
+/*check length of each one
+loop
+conditioonal .length
+New array if tru push
+
+concatenate new array
+loop*/
