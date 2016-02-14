55b8747ab5fa9186cbcb3f73d6bec3addc1dec3f
Diego Martinez <Diego.ma.martinez@gmail.com>
2016-02-14 16:45:42 -0500

    cheat sheet

--- jquery/cheatSheet/cheat.js
+++ jquery/cheatSheet/cheat.js
@@ -1,23 +1,37 @@
 $(document).ready(function(){
 	
-$( '.animateLink' ).click(function() {
-	// slide box up
-$("#animateMe").slideUp('slow', function(){
-	// slide box down
-$("#animateMe").slideDown(2000, function(){
-	// fade box out
-$("#animateMe").fadeOut('slow', function(){
-	// fade box in
-$("#animateMe").fadeIn('slow', function(){
-	// fill box color with green
-$("#animateMe").animate("div").css("background-Color","green"); 
+	$( '.animateLink' ).click(function() {
+		// slide box up
+	$("#animateMe").text("Slide Up").slideUp('slow', function(){
+		// slide box down
+	$("#animateMe").text("Slide Down").slideDown(2000, function(){
+		// fade box out
+	$("#animateMe").text("Fade Out").fadeOut('slow', function(){
+		// fade box in
+	$("#animateMe").text("Fade In").fadeIn('slow', function(){
+		// fill box color with green
+	$("#animateMe").text("Animate").animate({left:"+=200"}, 1000 ).animate({left:"-=200"}, 100 );
+		});
+		});
+		});
+		});
+	});
 
-	});
-	});
-	});
-	});
-	});
+	$( '.effUp' ).click(function() {
+		$("#animateMe").text("Slide Up Effect").css("font-size","3em").slideUp('slow', function(){
+		});
+		});
+		$( '.effDown' ).click(function() {
+		$("#animateMe").text("Slide Down Effect").slideDown(2000, function(){
+		});
+		});
+		$( '.effOut' ).click(function() {
+		$("#animateMe").text("Fade Out/In Effect").fadeOut('slow').fadeIn('fast',function(){
+		});
+		});
 
+		$( '.effAni' ).click(function() {
+		$("#animateMe").animate("div").text("Animate This").animate({left:"+=200"}, 1000 ).animate({left:"-=200"}, 100 );
+		});
+	
 });
-
-
