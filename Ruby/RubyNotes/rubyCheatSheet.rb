6edf729fb8e3b0aae756b64088616c37477ca4ed
Diego Martinez <Diego.ma.martinez@gmail.com>
2016-02-17 10:31:54 -0500

    Exceptions

--- Ruby/RubyNotes/rubyCheatSheet.rb
+++ Ruby/RubyNotes/rubyCheatSheet.rb
@@ -21,3 +21,34 @@ end
  	return num_1.to_i + num_2.to_i
  end
  puts add_nums(3,4)
+ #--------------------#
+
+ x = 1
+ def change_x(x)
+ 	x = 4
+ end
+ change_x(x)
+ puts "x = #{x}"
+
+ # out put is x = 1
+ # Variables are passed by value 
+ # outside variables do not effect internal variablesss and vise versa
+ # inside variables will not effect variables outside the definitions
+
+#--------------------
+#Exceptions
+begin 
+	answer = first_num / second_num
+rescue
+	puts" yououou"
+	exit
+end
+	puts "#{first_num} / #{second_num} = #{answer}"
+
+#Raise
+age = 12
+def check_(age)	
+	raise ArgumentError 
+	puts "Enter positive Num"
+end
+#---------------------------
