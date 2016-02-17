1ba713dd45140d2b275a15af1841b2216e9aa1b0
Diego Martinez <Diego.ma.martinez@gmail.com>
2016-02-17 10:07:48 -0500

    ruby notes

---
+++ Ruby/RubyNotes/rubyCheatSheet.rb
@@ -0,0 +1,20 @@
+
+# this outputs all the numbers in the array
+numbers = [1,2,3,4,5]
+
+for number in numbers
+  puts "#{numbers}"
+end
+
+# Another loop
+groceries = ["bananas","apple","pear"]
+froceries.each do |food| # |food| temporary hold of item in array
+  puts "some #{food}"
+end
+
+(0..10).each do |i|
+  puts "# #{i}"
+end
+
+# Functions
+ asdfasf
